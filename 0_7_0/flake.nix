63429f77b2d5aba3bec3391d38953bc5e0716cc9
hugosenari <hugosenari@gmail.com>
2022-03-19 03:56:30 -0300

    chore: re-index ../nimpkgs/e/eris/0_7_0

--- 0_7_0/flake.nix
+++ 0_7_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-eris-0_7_0.flake = false;
   inputs.src-eris-0_7_0.owner = "~ehmry";
-  inputs.src-eris-0_7_0.ref   = "refs/tags/0.7.0";
+  inputs.src-eris-0_7_0.ref   = "0.7.0";
   inputs.src-eris-0_7_0.repo  = "eris";
   inputs.src-eris-0_7_0.type  = "sourcehut";
   
